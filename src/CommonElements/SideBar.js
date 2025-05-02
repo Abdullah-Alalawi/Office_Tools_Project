f6bc0b774475a8e24693adf3da419a58a47ce453
Abdullah-Alalawi <alawaa0j@gmail.com>
2025-05-02 14:34:17 +0300

    Typo Fix

--- src/CommonElements/SideBar.js
+++ src/CommonElements/SideBar.js
@@ -5,6 +5,7 @@ import profileImage from "../assets/default-avatar.jpg";
 import { IoMdArrowDropdown } from "react-icons/io";
 import { TfiRulerPencil } from "react-icons/tfi";
 import { MdConstruction } from "react-icons/md";
+import { FaFeatherAlt } from "react-icons/fa";
 
 const SideBar = ({ buttons, userName, userType }) => {
   const [selectedButton, setSelectedButton] = useState("");
@@ -57,9 +58,9 @@ const SideBar = ({ buttons, userName, userType }) => {
     <div className="bg-white z-0 min-w-72 max-w-72 relative">
       <aside className="h-full border-r border-gray-200 flex flex-col">
         <div className="p-4 h-24 sm:h-16 flex items-center justify-center space-x-3 mt-4">
-        <MdConstruction className="mr-2 text-3xl text-TweetBlue" />
+
           <div className="font-semibold text-2xl text-TweetBlue">    
-          Sculpture <span className="text-textgray">Tools Kit </span>
+          <FaFeatherAlt size={25}/> Office <span className="text-textgray">Tools Kit </span>
           </div>
         </div>
 
@@ -75,7 +76,7 @@ const SideBar = ({ buttons, userName, userType }) => {
                     className={`text-start flex justify-start px-4 text-base min-w-full max-w-full py-7 focus:outline-none rounded-lg ${
                       selectedButton === button.label
                         ? "bg-TweetBlue text-white font-semibold"
-                        : "bg-transparent text-textlightgray font-normal"
+                        : "bg-transparent text-textlightgray font-normal hover:shadow-md hover:bg-gray-100"
                     }`}
                     onClick={() => handleButtonClick(button.label, button.path)}
                   >
